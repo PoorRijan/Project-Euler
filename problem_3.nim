dad1bd6a00fe3efd350bce23f0745fb8a6c67f49
PoorRijan <rijan_dhakal2055@protonmail.com>
2025-04-15 10:29:07 -0500

    Name changes and answer for question 3.
    I am not convinced this was the best solution for 3 but the answer is
    correct

---
+++ problem_3.nim
@@ -0,0 +1,24 @@
+import sequtils
+
+# Define a function to find if a number is a prime
+
+proc is_prime(x:int): bool = 
+
+    for y in 2 ..< x:
+        if x mod y == 0:
+            return false
+    return true
+
+
+# What is the number?
+var x: int = 600851475143
+
+var myArray: seq[int]
+
+for i in 2 ..< x:
+    if i mod 2 == 0:
+        continue
+    elif x mod i == 0 and is_prime(i):
+        myArray.add(i)
+
+echo myArray[^1]
