c5646f6c835415d5266ff82e636c8477ac81fd23
PoorRijan <rijan_dhakal2055@protonmail.com>
2025-05-05 10:38:47 -0500

    Problem 9

---
+++ problem_9.nim
@@ -0,0 +1,16 @@
+proc pythagoras_func(i:int, j:int, k:int): bool = 
+    
+    if i * i + j * j == k * k:
+        return true
+    false
+# For now going with a triple-nested for loop - I am ashamed.
+
+for i in 1 ..< 1000:
+    for j in 1 ..< 1000:
+        for k in 1 ..< 1000:
+            if i + j + k == 1000 and k > j and j > i:
+                if pythagoras_func(i,j,k):
+                    echo i, j, k
+                    echo i * j * k
+                    break
+
